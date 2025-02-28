879a80a3f0c0dc78138cc8830a773313a433766c
vinicciusnev <xxnashflows@hotmail.com>
2025-02-27 21:48:24 -0300

    chore: Adicionando arquivos iniciais

---
+++ vite-env.d.ts
@@ -0,0 +1,11 @@
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+   VITE_API_URL: string;
+   NODE_ENV: 'development' | 'production' | 'test';
+ }
+ 
+ interface ImportMeta {
+   readonly env: ImportMetaEnv;
+ }
+ 
